e9ff73d8c21c37bd7dacb80d500f4522898fc43d
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-01 16:45:32 +0000

    Success

--- intials/intials.c
+++ intials/intials.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
-    int number = get_int("Number is: ");
-    printf("%i\n", number);
+
+ string word = get_string("My name is: ");
+ printf("%c", toupper(word[0])); // prints the first initial of the name uppercase, using the toupper
+  for (int i = 0; i < strlen(word); i++) // this checks if the loop encounters a space, also if the index reaches the end of the string
+  {
+    if(word[i] == ' ')
+    {
+        printf("%c", toupper(word[i + 1])); // make the next character as an uppercase ch
+    }
+  }
+        printf("\n");
 }
