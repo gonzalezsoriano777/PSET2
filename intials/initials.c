0871175637b4eed89586717ff3983ff4d93bf98a
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-01 17:03:00 +0000

    Initials, uppercase first letter of names

--- intials/initials.c
+++ intials/initials.c
@@ -7,7 +7,7 @@ int main(void)
 {
 
     string word = get_string();
-    printf("%c", toupper(word[0])); // prints the first initial of the name uppercase, using the toupper
+    printf("%c", toupper(word[0]));  // prints the first initial of the name uppercase, using the toupper
     for (int i = 0; i < strlen(word); i++)
         // this checks if the loop encounters a space, also if the index reaches the end of the string
     {
